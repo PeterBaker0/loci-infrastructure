3dfdb222770e5de0fae91a3fa492c58faccc716c
Ben Leighton <Ben.Leighton@csiro.au>
2019-10-08 10:17:13 +1100

    https port

--- modules/loci-ec2-test/ec2-gateway.tf
+++ modules/loci-ec2-test/ec2-gateway.tf
@@ -65,6 +65,22 @@ resource "aws_security_group" "loci-ec2" {
     protocol    = "tcp"
     ipv6_cidr_blocks =["::/0"]
   }
+  
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 443 
+    to_port     = 443 
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  ingress {
+    # TLS (change to whatever ports you need)
+    from_port   = 443 
+    to_port     = 443 
+    protocol    = "tcp"
+    ipv6_cidr_blocks =["::/0"]
+  }
 
   egress {
     from_port       = 0
