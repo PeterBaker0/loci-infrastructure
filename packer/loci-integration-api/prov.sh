7bc40d3a838b020e9b7a4ff178e961bc8c93cada
Ben Leighton <Ben.Leighton@csiro.au>
2019-10-14 14:11:49 +1100

    point at caddy_auto branch

--- packer/loci-integration-api/prov.sh
+++ packer/loci-integration-api/prov.sh
@@ -7,5 +7,5 @@ chmod +x /usr/bin/docker-compose
 systemctl enable docker
 systemctl start docker && sleep 5
 usermod -a -G docker ec2-user
-git clone --single-branch --branch prod_compose https://github.com/CSIRO-enviro-informatics/loci-integration-api.git 
+git clone --single-branch --branch caddy_auto_https https://github.com/CSIRO-enviro-informatics/loci-integration-api.git 
 cd loci-integration-api && pwd && ls && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d 
