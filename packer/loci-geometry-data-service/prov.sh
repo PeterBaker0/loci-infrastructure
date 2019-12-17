cfd4eefc72557f862c3e1ed1e3b21edca5e4669d
Jonathan Yu <jonathan.yu@csiro.au>
2019-12-17 12:03:52 +1100

    adding packer config for gds

---
+++ packer/loci-geometry-data-service/prov.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+yum update -y
+amazon-linux-extras install docker 
+yum -y install git
+curl -L "https://github.com/docker/compose/releases/download/1.24.1/docker-compose-$(uname -s)-$(uname -m)" -o /usr/bin/docker-compose
+chmod +x /usr/bin/docker-compose
+systemctl enable docker
+systemctl start docker && sleep 5
+usermod -a -G docker ec2-user
+git clone --single-branch --branch master https://github.com/CSIRO-enviro-informatics/loci-geometry-data-service.git
+mv /tmp/instance.sh  /var/lib/cloud/scripts/per-instance/instance.sh
+chmod +x /var/lib/cloud/scripts/per-instance/instance.sh
+printenv
+cd /home/ec2-user/loci-geometry-data-service/ && pwd && ls && docker-compose -f docker-compose.yml up -d gservice
+
