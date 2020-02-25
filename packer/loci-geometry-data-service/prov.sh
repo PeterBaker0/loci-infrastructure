b174f0be658f36bd40e3ba8b13792dff31f4abb4
Jonathan Yu <jonathan.yu@csiro.au>
2020-02-25 12:18:26 +1100

    add ssl config for loci-geometry-data-service

--- packer/loci-geometry-data-service/prov.sh
+++ packer/loci-geometry-data-service/prov.sh
@@ -7,11 +7,14 @@ chmod +x /usr/bin/docker-compose
 systemctl enable docker
 systemctl start docker && sleep 5
 usermod -a -G docker ec2-user
-git clone --single-branch --branch master https://github.com/CSIRO-enviro-informatics/loci-geometry-data-service.git
+git clone --single-branch --branch prod https://github.com/CSIRO-enviro-informatics/loci-geometry-data-service.git
 mv /tmp/instance.sh  /var/lib/cloud/scripts/per-instance/instance.sh
 chmod +x /var/lib/cloud/scripts/per-instance/instance.sh
 printenv
 echo -e "GSDB_HOSTNAME=10.0.1.201" > /home/ec2-user/loci-geometry-data-service/.env
 echo -e "GSDB_PORT=25432" >> /home/ec2-user/loci-geometry-data-service/.env
 echo -e "PORT=80" >> /home/ec2-user/loci-geometry-data-service/.env
-cd /home/ec2-user/loci-geometry-data-service/ && pwd && ls && docker-compose -f docker-compose.yml up -d gservice
+mv /tmp/wildcard-loci-cat.bundle.pem /home/ec2-user/loci-geometry-data-service/certs/
+mv /tmp/wildcard-loci-cat.pem /home/ec2-user/loci-geometry-data-service/certs/
+ls -la /home/ec2-user/loci-geometry-data-service/certs/ 
+cd /home/ec2-user/loci-geometry-data-service/ && pwd && ls && docker-compose -f docker-compose.yml -f docker-compose.prod.yml up -d gservice caddy
