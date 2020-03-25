8b4b6de7f89c43825f552c0f9ef2939f856a6c6c
Ben Leighton <Ben.Leighton@csiro.au>
2020-03-25 15:02:13 +1100

    dual deployment

---
+++ environments/main2/main.tf
@@ -0,0 +1,19 @@
+module "loci-vpc-subnets" {
+    source = "../../modules/loci-vpc-subnets"
+    availability_zone = var.availability_zone
+}
+
+module "loci-ec2-test" {
+    source = "../../modules/loci-ec2-test"
+    loci-subnet-public = module.loci-vpc-subnets.loci-subnet-public
+    loci-subnet-private = module.loci-vpc-subnets.loci-subnet-private
+    public_key_path = var.public_key_path
+    public_key_suffix = var.public_key_suffix
+    private_key_path = var.private_key_path
+    triplestore_cache_url = var.triplestore_cache_url
+    cidr = var.cidr
+    ipv6_cidr = var.ipv6_cidr
+    eip_allocation_id = var.eip_allocation_id
+    loci-vpc = module.loci-vpc-subnets.loci-vpc
+    public_ip = var.public_ip
+}
