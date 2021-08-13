810787c95fab8c01e9293f8aba9447ab51becc17
Peter Baker <peter.baker@anu.edu.au>
2021-08-13 10:45:31 +1000

    updating scripts

--- ci_destroy.sh
+++ ci_destroy.sh
@@ -6,7 +6,7 @@ echo $new_id > action_id
 echo "ID:$current_id -> ID:$new_id"
 echo "Creating empty git commit with new tag" 
 git commit --allow-empty -m "Azure CI Tear Down:${new_id}."
-git tag $destroy_prefix:${new_id}
+git tag $destroy_prefix-${new_id}
 echo "Pushing tag and empty commit"
 git push --tag 
 git add version 
